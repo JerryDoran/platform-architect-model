6797f3f01a4103057c7c8bf4ae8b694498365265
Jerome Doran <disneyfan9184@gmail.com>
2025-07-27 12:34:32 -0400

    feat: integrate Arcjet for enhanced bot detection and signup protection

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,4 +1,33 @@
-import { auth } from "@/lib/auth"; // path to your auth file
-import { toNextJsHandler } from "better-auth/next-js";
- 
+import { auth } from '@/lib/auth';
+import ip from '@arcjet/ip';
+import arcjet, {
+  type ArcjetDecision,
+  type BotOptions,
+  type EmailOptions,
+  type ProtectSignupOptions,
+  type SlidingWindowRateLimitOptions,
+  detectBot,
+  protectSignup,
+  shield,
+  slidingWindow,
+} from '@arcjet/next';
+
+import { toNextJsHandler } from 'better-auth/next-js';
+
+const emailOptions = {
+  mode: 'LIVE',
+  block: ['DISPOSABLE', 'INVALID', 'NO_MX_RECORDS'],
+} satisfies EmailOptions;
+
+const botOptions = {
+  mode: 'LIVE',
+  allow: [],
+} satisfies BotOptions;
+
+const rateLimitOptions = {
+  mode: 'LIVE',
+  interval: '2m',
+  max: 5,
+} satisfies SlidingWindowRateLimitOptions<[]>;
+
 export const { POST, GET } = toNextJsHandler(auth);
